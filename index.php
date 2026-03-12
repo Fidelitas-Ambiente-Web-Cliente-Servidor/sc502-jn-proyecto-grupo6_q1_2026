d767fb7f2c877fa55f753a429b4e7bb4b98a2aed
Moraa <pavelmora21@gamil.com>
2026-03-12 17:31:28 -0600

    Agrego el index y corrijo el docker

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+<meta charset="UTF-8">
+<title>Plataforma de Salud</title>
+
+<link rel="stylesheet" href="css/estilos.css">
+
+</head>
+
+<body>
+
+<h1>Plataforma de Servicios de Salud</h1>
+
+<form action="php/login.php" method="POST">
+
+<label>Usuario</label>
+<input type="text" name="usuario" required>
+
+<label>Contraseña</label>
+<input type="password" name="password" required>
+
+<button type="submit">Ingresar</button>
+
+</form>
+
+</body>
+</html>
